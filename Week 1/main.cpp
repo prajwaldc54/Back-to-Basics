45493a439457e059db6b200a40a838cda547de27
Prajwal DC <prajwal.dc38@gmail.com>
2025-05-27 02:22:49 +0545

    Using directive and my first output

---
+++ Week 1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout; // using decrective and declaring for the whole program. 
+using std::cin;
+
+int main () {
+    cout << "Hello Its getting started";
+    return 0;
+}
