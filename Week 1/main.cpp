895f90eb28c391076ae78f86c6b3990307856dd1
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-08 07:15:51 +0545

    Variabale declaration and initialization and playing around

--- Week 1/main.cpp
+++ Week 1/main.cpp
@@ -3,6 +3,9 @@ using std::cout; // using decrective and declaring for the whole program.
 using std::cin;
 
 int main () {
+    int slices; // variable declaration
+    slices = 5; // variable initialization
+    cout<< "The Number of pizza Slices are "<< slices + 1 <<std::endl;
     cout << "Hello Its getting started";
     return 0;
 }
