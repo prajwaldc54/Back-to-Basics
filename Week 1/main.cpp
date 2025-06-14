0c3928fc0f4d08f800cab113f08e4f3e19148e00
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-15 03:44:18 +0545

    Using custom function instead of a cmath function(pow)

--- Week 1/main.cpp
+++ Week 1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <cmath> //Start to use fucntion built in math function. 
+// #include <cmath> //Start to use fucntion built in math function. 
 using std::cout; // using decrective and declaring for the whole program. 
 using std::cin;
+
+double pow (int , int); // Function declaration
 
 int main () {
     cout << "This is a basic pizza slices calculator. "<< std::endl;
@@ -11,6 +13,15 @@ int main () {
     cin>> slices; // taking input
     cout << "Whats the power you wanna add on the number of pizza slices: "<< std::endl;
     cin >> exponents_slices;
-    cout << "The power of the pizza slices are: " << pow (slices, exponents_slices) <<std::endl;
+    cout << "The power of the pizza slices are: " << pow (slices, exponents_slices) <<std::endl; //Function Call
     return 0;
+}
+
+double pow (int slices, int exponent_slices){ //Function Definition 
+    int result = 1;
+    for (int i = 0; i < exponent_slices; i++)
+    {
+        result = result * slices;
+    }
+    return result;
 }
