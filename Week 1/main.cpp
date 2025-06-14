ee11cab06eb2be5d7401763e705221c968d34e87
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-15 03:18:27 +0545

    Used cmath function and some cin stuff

--- Week 1/main.cpp
+++ Week 1/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <cmath> //Start to use fucntion built in math function. 
 using std::cout; // using decrective and declaring for the whole program. 
 using std::cin;
 
 int main () {
-    int slices; // variable declaration
-    slices = 5; // variable initialization
-    cout<< "The Number of pizza Slices are "<< slices + 1 <<std::endl;
-    cout << "Hello Its getting started";
+    cout << "This is a basic pizza slices calculator. "<< std::endl;
+    int slices; // variable declaration 
+    int exponents_slices; // power of the slices
+    cout<< "How many Pizza slices do you have you retard?"<< std::endl;
+    cin>> slices; // taking input
+    cout << "Whats the power you wanna add on the number of pizza slices: "<< std::endl;
+    cin >> exponents_slices;
+    cout << "The power of the pizza slices are: " << pow (slices, exponents_slices) <<std::endl;
     return 0;
 }
