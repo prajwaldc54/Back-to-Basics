b18b1bd242e592b59bea634effc17f9fe3300359
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-18 01:15:04 +0545

    Some more data types and constant as well a looking into escape sequence

--- Week 2/week2.cpp
+++ Week 2/week2.cpp
@@ -2,6 +2,7 @@
 #include <climits>
 using std::cout;
 using std::cin;
+#define Y 10
 void print_stuff(short a, int b, long c, long long d){ // using void fucntion as learned in last lecture to less populate the main function. 
     cout << SHRT_MAX<< std::endl;
     cout << SHRT_MIN<< std::endl;
@@ -16,11 +17,17 @@ void print_stuff(short a, int b, long c, long long d){ // using void fucntion as
     cout << LLONG_MIN<< std::endl;
     cout << ULLONG_MAX<< std::endl;
 }
-int main(){
+int main (){
     short a;
     int b;
     long c;
     long long d;
     print_stuff(a,b,c,d); //Not making main function too crowded for just printing the memory value.
-    return 0; 
+    const int y = 7;
+    char x = 'a'; // for char
+    bool is_found = false;
+    cout << (int) x << std::endl;
+    cout << std::boolalpha << is_found << std::endl; // using manipulator to print true or false
+    cout << Y << std::endl;
+    cout << y <<std::endl;
 }
