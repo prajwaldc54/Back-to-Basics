b0ddfdf90a641e2ecc8f550db77305632bad1d09
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-21 02:55:34 +0545

    Into the loops ICU concept and also created a program to find the factorial of a number

--- week 4/week4.cpp
+++ week 4/week4.cpp
@@ -6,42 +6,15 @@ using std::cin;
 using std::endl;
 
 int main (){
-    // string answer_name = "Prajwal";
-    // int answer_age = 25;
-    // string guess_name;
-    // cout << "Try guessing my name: ";
-    // cin>> guess_name;
-
-    // int guess_age;
-    // cout << "Second time lucky try guessing my age: ";
-    // cin >> guess_age;
-
-    // if (answer_name == guess_name && answer_age == guess_age) //Conditional flow to understand it.
-    // {
-    //     cout << "Buwaaay you are correct " << endl;
-    // }
-    // else{
-    //     cout << "Hard Luck! Try guessing me again" << endl;
-    // }
-
-    enum class Day {
-        Monday,
-        Tuesday,
-        Friday
-    };
-    Day guess_day = Day::Tuesday;
-    switch (guess_day)
+    int factorial_of;
+    cout << "Enter the number you want factorial of: \n";
+    cin>> factorial_of;
+    int fact = factorial_of;
+    for (int i = factorial_of - 1; i > 1; i--)
     {
-    case Day::Monday:
-        break;
-
-    case Day::Tuesday:
-        cout << "Good answer";
-        break;
-    
-    default:
-        cout << "Hard luck try again";
-        break;
+        factorial_of = factorial_of * i;
     }
+    
+    cout << "The factorial of " << fact << " is: " << factorial_of << endl;
     return 0; 
 }
