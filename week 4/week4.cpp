6de8a8991950d9fb80c5418ba586fc7c5390ba08
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-23 11:29:31 +0545

    Example of proper use of break in an integer type array

--- week 4/week4.cpp
+++ week 4/week4.cpp
@@ -6,36 +6,17 @@ using std::string;
 using std::endl;
 
 int main (){
-    // int factorial_of;
-    // cout << "Enter the number you want factorial of: \n";
-    // cin>> factorial_of;
-    // int fact = factorial_of;
-    // for (int i = factorial_of - 1; i > 1; i--)
-    // {
-    //     factorial_of = factorial_of * i;
-    // }
-    
-    // cout << "The factorial of " << fact << " is: " << factorial_of << endl;
-
-    // int factorial;
-    // cout << "Enter the number you want the factorial of: ";
-    // cin>> factorial;
-    // int fact = factorial;
-    // int i = factorial - 1;
-    // while (i > 0){
-    //     factorial *=i;
-    //     i --;
-    // };
-    // cout << "The factorial of " << fact << " is: " << factorial << endl;
-
-    string pass = "Nepal123";
-    string guess_pass;
-    do
+    int arr[5] = {100,70,89,-5,35};
+    bool found = false;
+    for (int i = 0; i < sizeof(arr); i++)
     {   
-         cout << "Enter my Password: \n";
-         std::getline(cin, guess_pass);
-    } while (guess_pass != pass);
-    cout << "Booyay you got it right.\n";
-
+        cout << arr[i] << " \n";
+        if (arr[i] < 0)
+        {
+            cout << "There is an invalild marks! Please check\n";
+            break;
+        }
+        
+    }
     return 0; 
 }
