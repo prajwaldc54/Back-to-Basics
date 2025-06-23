dbfb99157ea155e8c1fef633f0eac969aa3b2d6a
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-23 10:56:49 +0545

    Factorial using do while loop

--- week 4/week4.cpp
+++ week 4/week4.cpp
@@ -17,15 +17,30 @@ int main (){
     
     // cout << "The factorial of " << fact << " is: " << factorial_of << endl;
 
+    // int factorial;
+    // cout << "Enter the number you want the factorial of: ";
+    // cin>> factorial;
+    // int fact = factorial;
+    // int i = factorial - 1;
+    // while (i > 0){
+    //     factorial *=i;
+    //     i --;
+    // };
+    // cout << "The factorial of " << fact << " is: " << factorial << endl;
+
     int factorial;
-    cout << "Enter the number you want the factorial of: ";
-    cin>> factorial;
+    cout << "Enter any number you want factorial of: ";
+    cin >> factorial;
     int fact = factorial;
-    int i = factorial - 1;
-    while (i > 0){
+    int i =1;
+    do
+    {
         factorial *=i;
-        i --;
-    };
+        i ++;
+    } while (i < fact);
+    
     cout << "The factorial of " << fact << " is: " << factorial << endl;
+
+    
     return 0; 
 }
