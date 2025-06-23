675987b3a0cb5423dc956c9a6b02bb9419a3ac4c
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-24 00:28:36 +0545

    Guessing random number generated

--- Week 5/week5.cpp
+++ Week 5/week5.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using std::cout;
 using std::cin;
 using std::endl;
 
+void play_game(){
+    int random = rand() % 99 + 1;
+    while (true){
+    cout << "Guess the random number generated: \n";
+    int guess;
+    cin >> guess;
+    if (guess == random)
+    {
+        cout << "You got it brother!!!" << endl;
+        break;
+    }else if (guess > random) {
+        cout << "Its a lower value than you guessed" << endl;
+    } else if (guess < 0){
+        cout << "Invalid input only positive numbers" << endl;
+    } else {
+        cout << "Its a higher value than you guessed" << endl;
+    }
+    
+    }
+}
+
 int main (){
+    srand(time(0));
     cout << "Welcome to my console-based application.\n";
      int choice;
      do
@@ -16,7 +41,7 @@ int main (){
                         cout << "Thanks for playing\n";
                         break;
                     case 1: 
-                        cout << "Lets gooo!!!\n";
+                        play_game();
                         break;
     }
      } while (choice != 0);
