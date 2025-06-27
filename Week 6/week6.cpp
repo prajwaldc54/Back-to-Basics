46d51bcf962f906833b8c7801240b542d3c6483a
Prajwal DC <prajwal.dc38@gmail.com>
2025-06-27 23:57:30 +0545

    In a vector loop without appending

--- Week 6/week6.cpp
+++ Week 6/week6.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <fstream> 
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main (){
-    cout << "Enter any file you wanna create: \n";
-    std:: string file;
-    cin >> file;
+    // cout << "Enter any file you wanna create: \n";
+    // std:: string file;
+    // cin >> file;
 
-    std::ofstream name(file);
-    name << "Hello Guys into file system" << endl;
+    std::ofstream name("Basic.txt");
+    std::vector <std::string> vec;
+    vec.push_back("Prajwal");
+    vec.push_back("Manish");
+    vec.push_back("Rubin");
+    vec.push_back("Pankaj");
+
+    for (std::string n : vec)
+    {
+        name << n << endl;
+    }
+    
+    
 }
