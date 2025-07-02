9de7bf21e4e1617f955e075a47fad1030850ecda
Prajwal DC <prajwal.dc38@gmail.com>
2025-07-03 01:36:02 +0545

    Template function overloading

--- Week 8/week8.cpp
+++ Week 8/week8.cpp
@@ -14,12 +14,28 @@ void swap (T &a, T &b){
     b = temp;
 }
 
+template <typename T> //Overloading in function template
+void swap(T a[],T b[],int s){
+    for (int i = 0; i < s; i++)
+    {
+        T temp;
+        temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+    
+}
+
 int main() {
+    int const SIZE = 6;
     int a = 20;
     int b = 10;
 
     std::string fn = "Prajwal";
     std::string ln = "D C";
+
+    int eight[] = {8,8,8,8,8,8};
+    int zero[] = {0,0,0,0,0,0};
 
     cout << "Before value of a is " << a <<" and value of b is " << b << endl;
     cout << "Before First Name is " << fn <<" and Last Name is " << ln << endl;
@@ -29,5 +45,19 @@ int main() {
 
     swap(fn,ln);
     cout << "First Name " << fn <<" Last Name " << ln << endl;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << eight[i] <<zero[i] << " ";
+    }
+    cout << "\n";
+
+    swap(eight,zero,SIZE);
+    
+        for (int i = 0; i < SIZE; i++)
+    {
+        cout << eight[i] <<zero[i] << " ";
+    }
+    cout << "\n";
 
 } 
