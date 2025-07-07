f468462b6ba42066db227a73425a30f915fd1ff4
Prajwal DC <prajwal.dc38@gmail.com>
2025-07-08 01:47:38 +0545

    Accessing from private access modifier

--- Week 9/oop.cpp
+++ Week 9/oop.cpp
@@ -7,14 +7,17 @@ using std::endl;
 struct User {
     std::string first_name;
     std::string last_name;
-    std::string status;
+    std::string get_status(){  // getting data from private access modifier in a struct
+        return status;
+    }
+    private:
+    std::string status = "Platinum";
 }; 
 
 int main () {
     User user;
     user.first_name = "Prajwal";
     user.last_name = "D C";
-    user.status = "Platinum";
-
-    cout << "Data from struct are " << "Firstname: " << user.first_name << " Lastname: " << user.last_name << " Status: " << user.status << endl;
+    
+    cout << "Data from struct are " << "Firstname: " << user.first_name << " Lastname: " << user.last_name << " Status: " << user.get_status() << endl;
 }
