c807e51e19ca539ada19eaf6f340a48e6e2aecbe
Prajwal DC <prajwal.dc38@gmail.com>
2025-07-10 03:33:13 +0545

    Static data member usefulness

--- Week 9/oop.cpp
+++ Week 9/oop.cpp
@@ -6,8 +6,12 @@ using std::cin;
 using std::endl;
 
 class User {
-    std::string status = "Platinum";
+    std::string status;
+    static int user_count;
     public:
+    int get_user_count (){
+        return user_count;
+    }
         std::string first_name;
         std::string last_name;
         std::string get_status(){  // getting data from private access modifier in a struct
@@ -15,25 +19,31 @@ class User {
         }
 
         User () {
-
+            user_count++;
         }
 
-        // User (std::string n, std::string l, std::string s){
-        //         first_name = n;
-        //         last_name = l;
-        //         status = s;
-        // }
+        User (std::string n, std::string l, std::string s){
+                first_name = n;
+                last_name = l;
+                status = s;
+                user_count ++;
+        }
 
-        ~User (){}
+        ~User (){
+            user_count --;
+        }
 
         void set_status (std::string s) {
             if (s == "Gold" || s == "Silver" || s == "Bronze"){
                 status = s;
-                cout << "Matched";
+                cout << "Matched\n";
             }else
-            cout << "It doesnt maatch the status";
+            cout << "It doesnt maatch the status\n";
         }
 }; 
+
+int User::user_count = 000;
+
 
 // bool person_checker (std::vector <User>& users, User& u) {
 //     for (int i = 0; i < users.size(); i++)
@@ -47,8 +57,9 @@ class User {
 // }
 
 int main () {
-    User user4;
+    User user4, user1,user2,user3;
     user4.first_name = "Prajwal"; 
     user4.last_name = "D C";
-    user4.set_status("Silver");
+    user1.set_status("Gold");
+    cout << "Number of user: " << user1.get_user_count() << endl;
 }
